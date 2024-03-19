1675a6e5a195343fc203e5bd890dfb1f57a0d924
MochYusuf <yusufjabriko@gmail.com>
2024-03-19 21:22:38 +0700

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html", 'tentang.html'],
+    theme: {
+        extend: {}
+    },
+    plugins: ['prettier-plugin-tailwindcss'],
+}
